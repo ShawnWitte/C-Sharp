90282a4ef6f089ea8f5e698d5677cc440d0c4a65
unknown <witt0061@hz.nl>
2018-04-17 20:07:01 +0200

    Decorator exercise added

---
+++ WeatherStation/WeatherStation/WeatherData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    public class WeatherData : Subject
+    {
+        private ArrayList observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new ArrayList();
+        }
+
+        public void registerObserver(Observer o)
+        {
+            observers.Add(o);
+        }
+
+        public void removeObserver(Observer o)
+        {
+            int i = observers.IndexOf(o);
+            if(i >= 0)
+            {
+                observers.Remove(i);
+            }
+        }
+
+        public void notifyObserver()
+        {
+            for (int i = 0; i < observers.Count; i++)
+            {
+                Observer observer = (Observer)observers[i];
+                observer.update(temperature, humidity, pressure);
+            }
+        }
+
+        public void measurementsChanged()
+        {
+            notifyObserver();
+        }
+
+        public void setMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            measurementsChanged();
+        }
+
+
+    }
+}
+
